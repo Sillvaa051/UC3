020091bddbe5766f9a228c4e119dfcb466bc1fcb
Sillvaa051 <joaossilva008@gmail.com>
2025-12-12 19:20:37 -0300

    adicionado a pasta uc3

---
+++ ExercicioDeFixação.js
@@ -0,0 +1,19 @@
+/*
+1. Em que ano o banco de dados  foi desenvolvido?
+R: 1970 a 1972
+
+2. Quem é considerado o "pai dos banco de dados relacionais"?
+R: Edgar Frank Codd
+
+3. Explique a diferença de um banco de dados, um sistema gerenciador de banco de dados e um sistema de banco de dados.
+R: Banco de dados é um conjunto de dados armazenados, Sistema Gerenciador de Banco de Dados é o software que cria, manipula e controla o banco de dados e Sistema de Banco de Dados é todo o conjunto, incluindo banco de dados, SGBD etc..
+
+4. Quais são as principais características de um banco de dados?
+R: Entidade, Dado, Informação, Conhecimento
+
+5. Quais sãos diferentes níveis de abstração em um banco de dados.
+R: Nivel externo, Nivel Conceitual e  Nivel interno.
+
+6. Liste as vantagens de usar um banco de dados.
+R: Organização, Segurança e Integridade.
+*/
